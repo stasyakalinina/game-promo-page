f0f4eb6bf7871ec2837dde6e0e98323cd9614bc0
Anastasia Kalinina <stasya.kalinina@gmail.com>
2020-04-20 17:59:22 +0500

    прогресс-бар

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,3 +1,20 @@
-function multiple(first, second) {
-  return first * second;
+  // var progressLine = document.getElementById('progress-line');
+  // var progressAmount = document.getElementById('progress-number');
+
+function move() {
+  var elem = document.getElementById("progress-line");
+  var width = 10;
+  console.log('123');
+
+  var id = setInterval(frame, 10);
+  function frame() {
+    if (width >= 100) {
+      clearInterval(id);
+    } else {
+      width++;
+      elem.style.width = width + '%';
+    }
+  }
 }
+
+document.addEventListener('DOMContentLoaded', move);
--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,18 @@
-console.log(multiple(4, 6));
-console.log(multiple(2, 7));
+  function move() {
+    var elem = document.getElementById("progress-line");
+    var progressAmount = document.getElementById('progress-number');
+    var width = 0;
+
+    var id = setInterval(frame, 50);
+    function frame() {
+      if (width >= 50) {
+        clearInterval(id);
+      } else {
+        width++;
+        elem.style.width = width + '%';
+        progressAmount.textContent = Math.round(width / 10);
+      }
+    }
+  }
+
+  document.addEventListener('DOMContentLoaded', move);
